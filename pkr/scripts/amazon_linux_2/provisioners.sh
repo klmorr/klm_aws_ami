f0876ccea44e7a4352f2d3f45f1a81d180ba156e
klmorr <morrison.kris@outlook.com>
2022-01-28 17:25:04 -0600

    update templates, vars, scripts, file names

---
+++ pkr/scripts/amazon_linux_2/provisioners.sh
@@ -0,0 +1,3 @@
+sudo yum update -y
+sudo yum install python3 unzip ansible jq python-pip -y
+sudo pip install --upgrade pip
---
+++ pkr/scripts/windows_2016/Invoke-Provisioners.ps1
@@ -0,0 +1,65 @@
+function Install-Chocolatey
+{
+    [CmdletBinding()]
+    param()
+
+    Set-ExecutionPolicy Bypass -Scope Process -Force
+    [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072
+    Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
+
+    refreshenv
+}
+
+function Install-ChocolateyPackage
+{
+    [CmdletBinding()]
+    param(
+        [Parameter(
+            Position = 0,
+            Mandatory,
+            HelpMessage = "Chocolatey package name"
+        )]
+        [ValidateNotNullOrEmpty()]
+        [string]
+        $Name
+    )
+
+    $Command = "choco install $($Name) -y"
+
+    Invoke-Expression -Command $Command
+}
+
+function Invoke-PostBuild
+{
+    [CmdletBinding()]
+    param()
+
+    if ( Test-Path -Path  "C:\ProgramData\Amazon\EC2-Windows\Launch\Scripts\SendWindowsIsReady.ps1" )
+    {
+        Set-Location "C:\ProgramData\Amazon\EC2-Windows\Launch\Scripts"
+        .\SendWindowsIsReady.ps1 -Schedule
+        .\InitializeInstance.ps1 -Schedule
+        .\SysprepInstance.ps1 -NoShutdown
+    }
+
+    elseif ( Test-Path -Path "$ENV:ProgramFiles/Amazon/EC2Launch/EC2Launch.exe" )
+    {
+        $ec2LaunchExe = "$ENV:ProgramFiles/Amazon/EC2Launch/EC2Launch.exe"
+        & $ec2LaunchExe sysprep
+    }
+
+    else
+    {
+        Write-Output "Amazon launch config not found"
+    }
+}
+
+Write-Output "Installing chocolatey"
+
+Install-Chocolatey -Verbose
+
+Install-ChocolateyPackage -Name "awscli-session-manager" -Verbose
+
+Invoke-PostBuild
+
+

f96cd82069838510897f603f50775ae8d9b740e7
klmorr <morrison.kris@outlook.com>
2022-01-28 18:06:59 -0600

    add s3 default encryption

--- src/terraform/codebuild-ami-packer/main.tf
+++ src/terraform/codebuild-ami-packer/main.tf
@@ -2,13 +2,22 @@ resource "aws_s3_bucket" "main" {
   bucket        = "${var.prefix}-${var.project_name}"
   acl           = var.s3_bucket_acl
   force_destroy = true
+
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "AES256"
+      }
+    }
+  }
 }
 
 resource "aws_s3_bucket_public_access_block" "main" {
-  bucket              = aws_s3_bucket.main.id
-  block_public_acls   = true
-  block_public_policy = true
-  ignore_public_acls  = true
+  bucket                  = aws_s3_bucket.main.id
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
 }
 
 resource "aws_iam_role" "main" {
